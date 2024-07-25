d252aae0c32a41ff7d160490d9c92a93597a9d50
gerald-izuchukwu <geraldlouisugwunna@gmail.com>
2024-07-25 18:28:18 +0100

    Created Internet gateway to allow internet traffic into the vpc Added route public route table that routes to the igw, associated the route table to the pubic subnet

--- main.tf
+++ main.tf
@@ -16,8 +16,9 @@ resource "aws_vpc" "main" {
   }
 }
 
+# create subnet
 resource "aws_subnet" "public" {
-  count                   = 1
+  #   count                   = 1
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.0.0/24"
   availability_zone       = var.avail_zone
@@ -27,6 +28,38 @@ resource "aws_subnet" "public" {
     Name = "${var.env_prefix}_public_subnet-1"
   }
 }
+# create igw for for vpc
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "${var.env_prefix}_igw"
+  }
+
+}
+
+# create public route table
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  route {
+    cidr_block = "10.0.0.0/16"
+    gateway_id = "local"
+  }
+
+  tags = {
+    Name = "${var.env_prefix}_public_route_table"
+  }
+}
+
+# associate route table to subnet
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_route_table.id
+}
 
 
 
@@ -35,4 +68,54 @@ resource "aws_subnet" "public" {
 #     tags = {
 #         Name = "webserver-1"
 #     }
+# }
+
+# -------- below this is config for private subnet and 
+# NAT-GW but NAT-GW isnt free
+
+# resource "aws_subnet" "private" {
+#   vpc_id            = aws_vpc.main.id
+#   cidr_block        = "10.0.1.0/24"
+#   availability_zone = var.avail_zone
+
+#   tags = {
+#     Name = "${var.env_prefix}_private_subnet-1"
+#   }
+# }
+
+# resource "aws_nat_gateway" "this" {
+#   allocation_id = aws_eip.this.id
+#   subnet_id     = aws_subnet.public.id
+#   tags = {
+#     Name = "${var.env_prefix}_nat_gw"
+#   }
+#   depends_on = [aws_internet_gateway.this]
+# }
+
+# resource "aws_eip" "this" {
+#   domain = "vpc"
+#   tags = {
+#     Name = "${var.env_prefix}_nat_eip"
+#   }
+# }
+
+# resource "aws_route_table" "private_route_table" {
+#   vpc_id = aws_vpc.main.id
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_nat_gateway.this.id
+#   }
+#   route {
+#     cidr_block = "10.0.0.0/16"
+#     gateway_id = "local"
+#   }
+
+#   tags = {
+#     Name = "${var.env_prefix}_private_route_table"
+#   }
+# }
+
+# resource "aws_route_table_association" "private" {
+#   subnet_id      = aws_subnet.private.id
+#   route_table_id = aws_route_table.private_route_table.id
 # }
