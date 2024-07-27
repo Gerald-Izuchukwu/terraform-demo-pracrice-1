35a50cd98a2488f571ed1c2165e2a186d688ca8c
gerald-izuchukwu <geraldlouisugwunna@gmail.com>
2024-07-27 21:01:38 +0100

    added an instance removed the nacl because it prevented ssh-ing

--- main.tf
+++ main.tf
@@ -1,201 +1,228 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-variable "env_prefix" {}
-variable "avail_zone" {}
-variable "my_ip_address" {}
-variable "public_key_path" {
-  description = "Path to the ssh public key file"
-  type        = string
-  default     = "~/.ssh/id_rsa.pub"
-}
-
-# create VPC
-resource "aws_vpc" "main" {
-
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-  tags = {
-    Name = "${var.env_prefix}_vpc"
-  }
-}
-
-# create subnet
-resource "aws_subnet" "public" {
-  #   count                   = 1
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.0.0/24"
-  availability_zone       = var.avail_zone
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "${var.env_prefix}_public_subnet-1"
-  }
-}
-# create igw for for vpc
-resource "aws_internet_gateway" "this" {
-  vpc_id = aws_vpc.main.id
-  tags = {
-    Name = "${var.env_prefix}_igw"
-  }
-
-}
-
-# create public route table
-resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.main.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.this.id
-  }
-
-  route {
-    cidr_block = "10.0.0.0/16"
-    gateway_id = "local"
-  }
-
-  tags = {
-    Name = "${var.env_prefix}_public_route_table"
-  }
-}
-
-# associate route table to subnet
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public_route_table.id
-}
-
-resource "aws_network_acl" "this" {
-  vpc_id = aws_vpc.main.id
-  egress { //https traffic to the internet from the subnet
-    protocol   = "tcp"
-    rule_no    = 100
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 443
-    to_port    = 443
-  }
-  ingress {
-    protocol   = "tcp"
-    rule_no    = 100
-    action     = "allow"
-    cidr_block = "10.0.0.0/24"
-    from_port  = 0
-    to_port    = 65535
-  }
-
-  tags = {
-    Name = "${var.env_prefix}_nacl"
-  }
-}
-
-resource "aws_network_acl_association" "this" {
-  network_acl_id = aws_network_acl.this.id
-  subnet_id      = aws_subnet.public.id
-}
-
-resource "aws_security_group" "this" {
-  ingress { //for ssh
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [var.my_ip_address]
-  }
-
-  ingress { // for internet traffic to enter the webserver
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress { // for traffic to leave the intsnace regardless of protocol and ports
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "${var.env_prefix}_sg"
-  }
-}
-
-resource "aws_key_pair" "this" {
-  key_name   = "${var.env_prefix}_key_pair"
-  public_key = file(var.public_key_path)
-}
-
-
-# resource "aws_instance" "this" {
-#     ami_id = "ami-0b72821e2f351e396"
-#     tags = {
-#         Name = "webserver-1"
-#     }
+# provider "aws" {
+#   region = "us-east-1"
 # }
 
+# variable "env_prefix" {}
+# variable "avail_zone" {}
+# variable "my_ip_address" {}
+# variable "public_key_path" {
+#   description = "Path to the ssh public key file"
+#   type        = string
+# }
 
+# # create VPC
+# resource "aws_vpc" "main" {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-# -------- below this is config for private subnet and NAT-GW but NAT-GW isnt free
-
-# resource "aws_subnet" "private" {
-#   vpc_id            = aws_vpc.main.id
-#   cidr_block        = "10.0.1.0/24"
-#   availability_zone = var.avail_zone
-
+#   cidr_block           = "10.0.0.0/16"
+#   enable_dns_support   = true
+#   enable_dns_hostnames = true
 #   tags = {
-#     Name = "${var.env_prefix}_private_subnet-1"
+#     Name = "${var.env_prefix}_vpc"
 #   }
 # }
 
-# resource "aws_nat_gateway" "this" {
-#   allocation_id = aws_eip.this.id
-#   subnet_id     = aws_subnet.public.id
-#   tags = {
-#     Name = "${var.env_prefix}_nat_gw"
-#   }
-#   depends_on = [aws_internet_gateway.this]
-# }
+# # create subnet
+# resource "aws_subnet" "public" {
+#   #   count                   = 1
+#   vpc_id                  = aws_vpc.main.id
+#   cidr_block              = "10.0.0.0/24"
+#   availability_zone       = var.avail_zone
+#   map_public_ip_on_launch = true
 
-# resource "aws_eip" "this" {
-#   domain = "vpc"
 #   tags = {
-#     Name = "${var.env_prefix}_nat_eip"
+#     Name = "${var.env_prefix}_public_subnet-1"
 #   }
 # }
+# # create igw for for vpc
+# resource "aws_internet_gateway" "this" {
+#   vpc_id = aws_vpc.main.id
+#   tags = {
+#     Name = "${var.env_prefix}_igw"
+#   }
 
-# resource "aws_route_table" "private_route_table" {
+# }
+
+# # create public route table
+# resource "aws_route_table" "public_route_table" {
 #   vpc_id = aws_vpc.main.id
 #   route {
 #     cidr_block = "0.0.0.0/0"
-#     gateway_id = aws_nat_gateway.this.id
+#     gateway_id = aws_internet_gateway.this.id
 #   }
+
 #   route {
 #     cidr_block = "10.0.0.0/16"
 #     gateway_id = "local"
 #   }
 
 #   tags = {
-#     Name = "${var.env_prefix}_private_route_table"
+#     Name = "${var.env_prefix}_public_route_table"
 #   }
 # }
 
-# resource "aws_route_table_association" "private" {
-#   subnet_id      = aws_subnet.private.id
-#   route_table_id = aws_route_table.private_route_table.id
+# # associate route table to subnet
+# resource "aws_route_table_association" "public" {
+#   subnet_id      = aws_subnet.public.id
+#   route_table_id = aws_route_table.public_route_table.id
 # }
+
+# # resource "aws_network_acl" "this" {
+# #   vpc_id = aws_vpc.main.id
+# #   egress { //https traffic to the internet from the subnet
+# #     protocol   = "tcp"
+# #     rule_no    = 100
+# #     action     = "allow"
+# #     cidr_block = "0.0.0.0/0"
+# #     from_port  = 443
+# #     to_port    = 443
+# #   }
+
+# #   ingress {
+# #     protocol   = "tcp"
+# #     rule_no    = 100
+# #     action     = "allow"
+# #     cidr_block = "10.0.0.0/24"
+# #     from_port  = 0
+# #     to_port    = 65535
+# #   }
+
+# #     egress { //ssh
+# #     protocol   = "tcp"
+# #     rule_no    = 200
+# #     action     = "allow"
+# #     cidr_block = "0.0.0.0/0"
+# #     from_port  = 22
+# #     to_port    = 22
+# #   }
+
+
+# #   ingress {
+# #     protocol   = "tcp"
+# #     rule_no    = 200
+# #     action     = "allow"
+# #     cidr_block = "0.0.0.0/0"
+# #     from_port  = 22
+# #     to_port    = 22
+# #   }
+
+# #   tags = {
+# #     Name = "${var.env_prefix}_nacl"
+# #   }
+# # }
+
+# # resource "aws_network_acl_association" "this" {
+# #   network_acl_id = aws_network_acl.this.id
+# #   subnet_id      = aws_subnet.public.id
+# # }
+
+# resource "aws_security_group" "this" {
+#   vpc_id = aws_vpc.main.id
+#   ingress { //for ssh
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = [var.my_ip_address]
+#   }
+
+#   ingress { // for internet traffic to enter the webserver
+#     from_port   = 8080
+#     to_port     = 8080
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   egress { // for traffic to leave the intsnace regardless of protocol and ports
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   tags = {
+#     Name = "${var.env_prefix}_sg"
+#   }
+# }
+
+# resource "aws_key_pair" "this" {
+#   key_name   = "${var.env_prefix}_key_pair"
+#   public_key = file(var.public_key_path)
+# }
+
+
+# resource "aws_instance" "this" {
+#   count                       = 1
+#   ami                         = "ami-0b72821e2f351e396"
+#   instance_type               = "t2.micro"
+#   key_name                    = aws_key_pair.this.key_name
+#   availability_zone           = var.avail_zone
+#   subnet_id                   = aws_subnet.public.id
+#   associate_public_ip_address = true
+#   vpc_security_group_ids      = [aws_security_group.this.id]
+#   tags = {
+#     Name = "webserver_1"
+#   }
+# }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # -------- below this is config for private subnet and NAT-GW but NAT-GW isnt free
+
+# # resource "aws_subnet" "private" {
+# #   vpc_id            = aws_vpc.main.id
+# #   cidr_block        = "10.0.1.0/24"
+# #   availability_zone = var.avail_zone
+
+# #   tags = {
+# #     Name = "${var.env_prefix}_private_subnet-1"
+# #   }
+# # }
+
+# # resource "aws_nat_gateway" "this" {
+# #   allocation_id = aws_eip.this.id
+# #   subnet_id     = aws_subnet.public.id
+# #   tags = {
+# #     Name = "${var.env_prefix}_nat_gw"
+# #   }
+# #   depends_on = [aws_internet_gateway.this]
+# # }
+
+# # resource "aws_eip" "this" {
+# #   domain = "vpc"
+# #   tags = {
+# #     Name = "${var.env_prefix}_nat_eip"
+# #   }
+# # }
+
+# # resource "aws_route_table" "private_route_table" {
+# #   vpc_id = aws_vpc.main.id
+# #   route {
+# #     cidr_block = "0.0.0.0/0"
+# #     gateway_id = aws_nat_gateway.this.id
+# #   }
+# #   route {
+# #     cidr_block = "10.0.0.0/16"
+# #     gateway_id = "local"
+# #   }
+
+# #   tags = {
+# #     Name = "${var.env_prefix}_private_route_table"
+# #   }
+# # }
+
+# # resource "aws_route_table_association" "private" {
+# #   subnet_id      = aws_subnet.private.id
+# #   route_table_id = aws_route_table.private_route_table.id
+# # }
